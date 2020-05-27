ec98798d7b7a60227ef8bd0b42c11ca5759c3f36
pytelaa <apy@bec.dk>
2020-05-27 14:45:38 +0200

    task 1 completed

--- task_one/src/main/java/dk/bec/poland/aa/logging/task_one/Entity.java
+++ task_one/src/main/java/dk/bec/poland/aa/logging/task_one/Entity.java
@@ -1,5 +1,8 @@
 package dk.bec.poland.aa.logging.task_one;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,6 +12,7 @@ import java.util.Random;
 
 public final class Entity {
   //TODO: add Logger here
+  private static final Logger log = LogManager.getLogger(Entity.class);
   private final int id;
   private final String category;
 
@@ -18,8 +22,15 @@ public final class Entity {
   }
 
   // TODO: create toString() implementation here
+    @Override
+    public String toString() {
+        return "Entity{" +
+                "id=" + id +
+                ", category='" + category + '\'' +
+                '}';
+    }
 
-  // TODO: add logging to this method
+    // TODO: add logging to this method
   public String randString() throws IOException {
     String url = "https://random-word-api.herokuapp.com/";
 
@@ -30,21 +41,29 @@ public final class Entity {
     String response;
 
     if (urlConnection.getResponseCode() == 200) {
+        log.trace("Response was equal to 200.");
       try (BufferedReader reader =
           new BufferedReader(new InputStreamReader(urlConnection.getInputStream()))) {
         response = reader.readLine();
+        log.trace("Read line was successful. Here is a response:");
+        log.trace(response);
       }
     } else {
+        log.error("Response was equal to: " +  urlConnection.getResponseCode());
       throw new RuntimeException("Failed to connect");
     }
 
     String serialized = serialize(response);
+    log.trace("Output of the serialize method");
+    log.trace(serialized);
     int randomIndex = new Random().nextInt(10);
+    log.trace("Random index was set to: " + randomIndex);
     return serialized.split(",")[randomIndex];
   }
 
   //TODO:  add logging to this method
   private static String serialize(String rawString) {
+      log.trace("Serialization method for argument: " + rawString);
     return rawString.replace("[", "").replace("]", "").replace("\"", "");
   }
 }
